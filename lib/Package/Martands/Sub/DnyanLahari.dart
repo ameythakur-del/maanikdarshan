7480089cde200683e1d716bff01ecabc8e79dffd
Hrushikesh <ubalehrushikesh18@gmail.com>
2022-07-02 19:21:36 +0530

    Update

---
+++ lib/Package/Martands/Sub/DnyanLahari.dart
@@ -0,0 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:flutter/material.dart';
+import 'package:maanikdarshan/Package/Martands/Sub/MartandsModel.dart';
+
+class DnyanLahari extends StatelessWidget {
+  const DnyanLahari({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    CollectionReference users = FirebaseFirestore.instance.collection('DnyanLahari');
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color(0xFF7F1B0E),
+        title: Text(
+          "ज्ञानलहरी",
+          style: TextStyle(fontFamily: 'Mukta', fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: FutureBuilder<DocumentSnapshot>(
+        future: users.doc("data").get(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text("Something went wrong"));
+          }
+
+          if (snapshot.hasData && !snapshot.data!.exists) {
+            return const Center(child: Text("Data does not exist"));
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            Map<String, dynamic> dataObj =
+            snapshot.data!.data() as Map<String, dynamic>;
+
+            var data = dataObj["data"];
+            return ListView.builder(
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  return MartandsModel(
+                    title: data[index]["title"],
+                    text: data[index]['text'].replaceAll("\\n", "\n"),
+                  );
+                });
+          }
+
+          return const Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
+  }
+}
